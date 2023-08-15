eef3098a06332195b72a29ad5c47ba6dd32bef0f
Mutmainah Odunewu <odunewumainah@gmail.com>
2023-08-15 17:06:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Home from './components/machineHome'
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <h1>Mutmainah</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/machineHome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function machineHome() {
+  return (
+    <div>
+        <h1>Mutmainah</h1>
+    </div>
+  )
+}
+
+export default machineHome
